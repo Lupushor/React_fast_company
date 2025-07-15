858db64602fdcf3fac41f0a1b9ed3f55b0d5b890
Aleksandr Lupu <103314959+Lupushor@users.noreply.github.com>
2025-07-15 10:45:10 +0200

    login Auth

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,80 +1,94 @@
 import React, { useEffect, useState } from "react";
-// import { validator } from "../../utils/validator";
+import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
 import CheckBoxField from "../common/form/checkBoxField";
-import * as yup from "yup";
-// import { matches, set } from "lodash";
+// import * as yup from "yup";
+import { useAuth } from "../../hooks/useAuth";
+import { useHistory } from "react-router-dom";
 
 const LoinForm = () => {
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   const [data, setData] = useState({ email: "", password: "", stayOn: false });
   const [errors, setErrors] = useState({});
+  const [enterErrorr, setEnterError] = useState(null);
+
+  const history = useHistory();
+
+  const { logIn } = useAuth();
 
   const handleChange = (target) => {
     setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    setEnterError(null);
   };
 
-  const validateSchema = yup.object().shape({
-    password: yup
-      .string()
-      .required("Пароль обязателен для заполнения")
-      // (вместо .matches(/.{8,}/) используем встроенный метод .min())
-      .min(8, "Пароль должен быть не короче 8 символов")
-      // Заглавная буква
-      .matches(/[A-Z]/, "Пароль должен содержать хотя бы одну заглавную букву")
-      // Цифра
-      .matches(/\d/, "Пароль должен содержать хотя бы одну цифру")
-      // Спецсимвол
-      .matches(
-        /[!@#$%^&*]/,
-        "Пароль должен содержать хотя бы один из специальных символов !@#$%^&*"
-      ),
-    email: yup
-      .string()
-      .email("Email is not correct")
-      .required("Email is required"),
-  });
+  // const validateSchema = yup.object().shape({
+  //   password: yup
+  //     .string()
+  //     .required("Пароль обязателен для заполнения")
+  //     // (вместо .matches(/.{8,}/) используем встроенный метод .min())
+  //     .min(8, "Пароль должен быть не короче 8 символов")
+  //     // Заглавная буква
+  //     .matches(/[A-Z]/, "Пароль должен содержать хотя бы одну заглавную букву")
+  //     // Цифра
+  //     .matches(/\d/, "Пароль должен содержать хотя бы одну цифру")
+  //     // Спецсимвол
+  //     .matches(
+  //       /[!@#$%^&*]/,
+  //       "Пароль должен содержать хотя бы один из специальных символов !@#$%^&*"
+  //     ),
+  //   email: yup
+  //     .string()
+  //     .email("Email is not correct")
+  //     .required("Email is required"),
+  // });
 
-  // const validatorConfig = {
-  //   email: {
-  //     isRequired: { message: "Email is required" },
-  //     isEmail: { message: "Email is not correct" },
-  //   },
-  //   password: {
-  //     isRequired: { message: "Password is required" },
-  //     isCapitalSymbol: {
-  //       message: "Password must contain at least one capital letter",
-  //     },
-  //     isContainDigit: { message: "Password must contain at least one digit" },
-  //     min: { message: "Password must contain at least 8 characters", value: 8 },
-  //   },
-  // };
+  const validatorConfig = {
+    email: {
+      isRequired: { message: "Email is required" },
+      // isEmail: { message: "Email is not correct" },
+    },
+    password: {
+      isRequired: { message: "Password is required" },
+      // isCapitalSymbol: {
+      //   message: "Password must contain at least one capital letter",
+      // },
+      // isContainDigit: { message: "Password must contain at least one digit" },
+      // min: { message: "Password must contain at least 8 characters", value: 8 },
+    },
+  };
 
   useEffect(() => {
     validate();
   }, [data]);
 
   const validate = () => {
-    // const errors = validator(data, validatorConfig);
-    validateSchema
-      .validate(data)
-      .then(() => setErrors({}))
-      .catch((err) => setErrors({ [err.path]: err.message }));
-    // setErrors(errors);
+    const errors = validator(data, validatorConfig);
+    // validateSchema
+    //   .validate(data)
+    //   .then(() => setErrors({}))
+    //   .catch((err) => setErrors({ [err.path]: err.message }));
+    setErrors(errors);
     return Object.keys(errors).length === 0;
   };
 
   const isValid = Object.keys(errors).length === 0;
 
-  const handleSubmit = (e) => {
-    console.log(e);
-
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const isValidate = validate();
     if (!isValidate) return;
 
-    console.log(data);
+    try {
+      // await logIn({
+      //   email: data.email,
+      //   password: data.password,
+      // });
+      await logIn(data);
+      history.push("/"); // Redirect to home page after successful login
+      // setData({ email: "", password: "", stayOn: false });
+    } catch (error) {
+      setEnterError(error.message);
+    }
   };
 
   return (
@@ -98,10 +112,11 @@ const LoinForm = () => {
       <CheckBoxField value={data.stayOn} onChange={handleChange} name="stayOn">
         Оставаться в системе
       </CheckBoxField>
+      {enterErrorr && <p className="alert alert-danger">{enterErrorr}</p>}
       <button
         className="btn btn-primary w-100 mx-auto"
         type="submit"
-        disabled={!isValid}
+        disabled={!isValid || enterErrorr}
       >
         Login
       </button>
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -5,7 +5,12 @@ import userService from "../services/user.service";
 import { toast } from "react-toastify";
 import { setTokens } from "../services/localStorege.service";
 
-const httpAuth = axios.create();
+const httpAuth = axios.create({
+  baseURL: "https://identitytoolkit.googleapis.com/v1/",
+  params: {
+    key: process.env.REACT_APP_FIREBASE_API_KEY,
+  },
+});
 
 const AuthContext = React.createContext();
 
@@ -17,10 +22,42 @@ const AuthProvider = ({ children }) => {
   const [currentUser, setUser] = useState({});
   const [error, setError] = useState(null);
 
-  async function signUp({ email, password, ...rest }) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`;
+  async function logIn({ email, password }) {
     try {
-      const { data } = await httpAuth.post(url, {
+      const { data } = await httpAuth.post(`accounts:signInWithPassword`, {
+        email,
+        password,
+        returnSecureToken: true,
+      });
+
+      setTokens(data);
+
+      // const { content } = await userService.getByEmail(email);
+      // setUser(content);
+    } catch (error) {
+      errorCatcher(error);
+      const { code, message } = error.response.data.error;
+      console.log(code, message);
+      if (code === 400 && message === "INVALID_LOGIN_CREDENTIALS") {
+        throw new Error("Email or password is incorrect");
+      }
+
+      if (code === 400 && message === "TOO_MANY_ATTEMPTS_TRY_LATER") {
+        throw new Error("User is disabled");
+      }
+
+      // if (code === 400 && message === "INVALID_PASSWORD") {
+      //   const errorObject = {
+      //     password: "Password is incorrect",
+      //   };
+      //   throw errorObject;
+      // }
+    }
+  }
+
+  async function signUp({ email, password, ...rest }) {
+    try {
+      const { data } = await httpAuth.post(`accounts:signUp`, {
         email,
         password,
         returnSecureToken: true,
@@ -37,13 +74,14 @@ const AuthProvider = ({ children }) => {
       // Here you would typically handle the sign-up logic, e.g., API call
     } catch (error) {
       errorCatcher(error);
-      const { code, message } = error.response.data.error;
-      if (code === 400 && message === "EMAIL_EXISTS") {
-        const errorObject = {
-          email: "Email is already exist",
-        };
-        throw errorObject;
-      }
+    }
+
+    const { code, message } = error.response.data.error;
+    if (code === 400 && message === "EMAIL_EXISTS") {
+      const errorObject = {
+        email: "Email is already exist",
+      };
+      throw errorObject;
     }
   }
 
@@ -71,7 +109,7 @@ const AuthProvider = ({ children }) => {
   }, [error]);
 
   return (
-    <AuthContext.Provider value={{ signUp, currentUser }}>
+    <AuthContext.Provider value={{ signUp, logIn, currentUser }}>
       {children}
     </AuthContext.Provider>
   );
